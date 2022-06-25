d1e504cd66868f8cc6674acf27a252912dc4d8ef
cantorxu <xuxiangrui15@163.com>
2022-06-25 10:16:36 +0800

    CMAKE_MODULE_PATH test successfully!

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BACKWARD_HAS_DW 1
+#include "backward.hpp"
+namespace backward {
+backward::SignalHandling sh;
+}
 
 int main() {
   char *c = "hello world";
